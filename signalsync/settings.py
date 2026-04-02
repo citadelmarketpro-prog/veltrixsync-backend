df278bf70a18a961fad2da6121b08c4210ad6d26
ghostpete <ghostcoderme@gmail.com>
2026-04-02 01:01:32 +0100

    added semi final stage

--- signalsync/settings.py
+++ signalsync/settings.py
@@ -77,21 +77,21 @@ WSGI_APPLICATION = "signalsync.wsgi.application"
 # Database
 # ─────────────────────────────────────────────────────────────────────────────
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-#         conn_max_age=600
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
+}
 
 
 
